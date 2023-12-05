63b42e937cba5b14b4c44f9e0fb76bdc9b87a1b0
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2023-12-05 23:06:09 +0700

    Tambah HalamanEntry DestinasiEntry

---
+++ app/src/main/java/com/example/ativity9/ui/theme/Halaman/HalamanEntry.kt
@@ -0,0 +1,8 @@
+package com.example.ativity9.ui.theme.Halaman
+
+import com.example.ativity9.Navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
