8a98656a316bcfeff93516fd0dc46f908fc4cb7d
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2023-12-05 21:10:45 +0700

    Tambah PenyediaViewModel

--- app/src/main/java/com/example/ativity9/Model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ativity9/Model/PenyediaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.ativity9.Model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ativity9.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.AplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiSiswa)
