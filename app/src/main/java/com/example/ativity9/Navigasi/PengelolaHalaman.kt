5029b99bae76cbc2fe0e729b4342d84f2c3cd369
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2023-12-06 01:45:41 +0700

    Tambah PengelolaHalaman HostNavigasi

--- app/src/main/java/com/example/ativity9/Navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/ativity9/Navigasi/PengelolaHalaman.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.ativity9.R
@@ -42,4 +43,12 @@ fun SiswaTopAppBar(
                 }
             }
         })
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavController,
+    modifier: Modifier = Modifier
+){
+
 }
