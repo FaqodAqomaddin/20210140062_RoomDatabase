2c3453c6f507d51aae5acb07e67a078b8c4bddbe
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2023-12-05 23:53:26 +0700

    Tambah HalamanHome DestinasiHome

---
+++ app/src/main/java/com/example/ativity9/ui/theme/Halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.ativity9.ui.theme.Halaman
+
+import com.example.ativity9.Navigasi.DestinasiNavigasi
+import com.example.ativity9.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
